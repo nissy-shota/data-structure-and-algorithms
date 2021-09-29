98df8169ebe75b44f1b77f9654f282f2a7fe34d4
nissy-shota <shota_4410@icloud.com>
2021-09-29 14:38:53 +0900

    bogo sort

---
+++ sort/bogo.py
@@ -0,0 +1,22 @@
+'''
+適当に並べ替えてソートする．結構雑なSort？
+非常に時間のかかるアルゴリズム．ランダムに並び替えるだけなので．．．
+'''
+import random
+from typing import List
+
+def in_order(numbers: List[int]) -> bool:
+    for i in range(len(numbers) - 1):
+        if numbers[i] > numbers[i+1]:
+            return False
+    return True
+
+def bogo_sort(numbers: List[int]) -> List[int]:
+    while not in_order(numbers):
+        random.shuffle(numbers)
+    return numbers
+
+if __name__ == '__main__':
+    numbers = [random.randint(0,1000) for _ in range(10)]
+    print(f'input numbers: {numbers}')
+    print(f'bogo sorted: {bogo_sort(numbers)}')
