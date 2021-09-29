54fa69174683ea6f04e7746b7025fdebb11457cf
nissy-shota <shota_4410@icloud.com>
2021-09-29 19:15:19 +0900

    bogo sort

--- sort/bogo_sort.py
+++ sort/bogo_sort.py
@@ -1,6 +1,7 @@
 import random
 from typing import List
 
+
 def in_order(numbers: List[int]) -> bool:
     # for i in range(len(numbers) - 1):
     #     if numbers[i] > numbers[i+1]:
@@ -8,10 +9,12 @@ def in_order(numbers: List[int]) -> bool:
     # return True
     return all(numbers[i] <= numbers[i+1] for i in range(len(numbers)-1))
 
+
 def bogo_sort(numbers: List[int]) -> List[int]:
     while not in_order(numbers):
         random.shuffle(numbers)
     return numbers
+
 
 if __name__ == '__main__':
     numbers = [random.randint(0,1000) for _ in range(10)]
